0ca8a455a6a36add3cf1a65bf4ae3255a976e163
Roy <loyi.ho@foxmail.com>
2020-07-25 13:26:53 +0800

    美丽

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Word!");
-		System.out.println("H你好呀ello 世 界");
+		System.out.println("H你好呀ello 美丽世 界");
 	}
 }
