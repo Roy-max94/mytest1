85b231ac3f15ae38b1dcec575b4f1334c2f65660
Roy <loyi.ho@foxmail.com>
2020-07-25 13:28:34 +0800

    四面

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Word!");
-		System.out.println("你好 Word!");
+		System.out.println("Hello你好 Word!四面");
 	}
 }
