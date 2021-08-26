4709faaa92ec8dbd4f671893352e933b82a51237
Roy <loyi.ho@foxmail.com>
2021-08-26 12:30:17 +0800

    第一次修改.你好，四面。改为，你好，世界！

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Word!");
-		System.out.println("Hello你好 Word!四面");
+		System.out.println("Hello你好 Word!世界！");
 	}
 }
